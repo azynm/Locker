5fe0e4e5745937a9eff12ee50f2cb8988924b922
zayn <z4ynmaliik@gmail.com>
2025-09-04 17:57:17 +0100

    first commit

---
+++ src/main/java/com/example/authproj/AuthprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.authproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authproj/AuthprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
