48c716961d8d971d0fb3a8dcc09a2e1fef79270e
zayn <z4ynmaliik@gmail.com>
2025-09-12 22:19:43 +0100

    added findbyemail method

--- src/main/java/com/example/authproj/repositories/UserRepository.java
+++ src/main/java/com/example/authproj/repositories/UserRepository.java
@@ -6,5 +6,6 @@ import com.example.authproj.entity.User;
 
 public interface  UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    User findByEmail(String email);
 
 }
