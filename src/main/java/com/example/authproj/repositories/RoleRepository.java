e5e9caf8ae79c5dde1dcae7550a8eda049129724
zayn <z4ynmaliik@gmail.com>
2025-09-12 21:59:24 +0100

    commment deletion

--- src/main/java/com/example/authproj/repositories/RoleRepository.java
+++ src/main/java/com/example/authproj/repositories/RoleRepository.java
@@ -1,7 +1,7 @@
 package com.example.authproj.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.authproj.entity.Role; // Adjust the package path to where the Role class is
+import com.example.authproj.entity.Role; 
 
 public interface  RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
