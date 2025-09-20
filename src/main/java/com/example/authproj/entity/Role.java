428d7298190e959d6a711d00de62d45764a5c57b
zayn <z4ynmaliik@gmail.com>
2025-09-20 15:27:59 +0100

    updated entities to work with database

--- src/main/java/com/example/authproj/entity/Role.java
+++ src/main/java/com/example/authproj/entity/Role.java
@@ -1,7 +1,20 @@
 package com.example.authproj.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    
+    @Column(nullable = false, unique = true)
     String name;
 
     public Role() {
@@ -14,9 +27,14 @@ public class Role {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/example/authproj/entity/User.java
+++ src/main/java/com/example/authproj/entity/User.java
@@ -2,11 +2,39 @@ package com.example.authproj.entity;
 
 import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    
+    @Column(nullable = false, unique = true)
     String username;
+    
+    @Column(nullable = false)
     String password;
+    
+    @Column(nullable = false, unique = true)
     String email;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles;
 
     public User() {
@@ -37,18 +65,27 @@ public class User {
         return roles;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
     public void addRole(Role role) {
         this.roles.add(role);
     }
+    
     public void setUsername(String username) {
         this.username = username;
     }
+    
     public void setPassword(String password) {
         this.password = password;
     }
+    
     public void setEmail(String email) {
         this.email = email;
     }
-
-
 }
