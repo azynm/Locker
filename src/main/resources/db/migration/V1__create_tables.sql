bbafaef1d73945db085abdd2de1a497f54848e52
zayn <z4ynmaliik@gmail.com>
2025-09-20 15:23:36 +0100

    fixed sql

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -6,13 +6,14 @@ create table role (
 create table users (
     id serial primary key,
     username varchar(50) not null unique,
-    password varchar(100) not null,
+    email varchar(100) not null unique,
+    password varchar(100) not null
 );
 
 create table user_roles (
     user_id int references users(id) on delete cascade,
     role_id int references role(id) on delete cascade,
-    primary key (user_id, role_id)
+    primary key (user_id, role_id),
     constraint fk_user foreign key (user_id) references users(id) on delete cascade,
     constraint fk_role foreign key (role_id) references role(id) on delete cascade
 );
